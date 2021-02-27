06dc53b84dca6b4827959b2d83bca473afe81774
Bella Santiago <bellasantiago.adl@gmail.com>
2021-02-27 12:53:45 +1030

    Server backbone setup

---
+++ server.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+const express = require('express');
+
+// EXPRESS CONFIGURATION
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+// LISTENER
+// The below code effectively "starts" our server
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
