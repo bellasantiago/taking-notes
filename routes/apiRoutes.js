93837233497fecbb47e0782167d6eb088e5dcd57
Bella Santiago <bellasantiago.adl@gmail.com>
2021-03-08 11:46:30 +1030

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const noteData = require('../db/noteData.js');
 module.exports = (app) => {
     // API GET Requests
     app.get('/notes', function (req, res) {
-        saveData
+        noteData
             .retrieveNotes()
             .then(notes => res.json(notes))
             .catch(err => res.status(500).json(err));
@@ -12,7 +12,7 @@ module.exports = (app) => {
     // API POST Requests
 
     app.post('/notes', (req, res) => {
-        saveData
+        noteData
             .addNote(req.body)
             .then((note) => res.json(note))
             .catch(err => res.status(500).json(err));
